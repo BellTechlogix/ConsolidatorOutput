f6775e78c09db010dedecd8b9197d4cc81c19a69
Kristopher Roy <kristopher.roy@gmail.com>
2022-08-22 18:48:26 -0700

    swaped around email and set id as email, also fixed folder/file format

--- ConsolidatorConversion.ps1
+++ ConsolidatorConversion.ps1
@@ -2,7 +2,7 @@
 	ConsolidatorConversion.ps1
 	Created By - Kristopher Roy
 	Created On - Aug 10 2021
-	Modified On - 
+	Modified On - Aug 22 2021
 
 	This Script grabs the output of consolidator moves the fields around for Kingland input and sets them to lowercase
 #>
@@ -13,11 +13,11 @@ $filename = 'consolidator.csv'
 #FTPVariables
 $username = "test"
 $password = "123qwe"
-$local_file = $path$filename
+$local_file = $path+$filename
 $remote_file = "ftp://192.168.2.222/PROD_HR.Import.csv"
  
 #grab the csv swap the UserID and Email fields and then make both lowercase
-$ConsolidatorUsers = import-csv $path$filename|select BusUnit,ComplianceRegion,DefaultPassword,Dept,Effective_Date,@{N='UserID';E={$_.UserID.ToLower()}},Employee_ID,FirstName,HireDate,JobCode,Title,LastName,MiddleName,TaxID,Status,SupervisorID,@{N='EmailAddress';E={$_.EmailAddress.ToLower()}},LocationDesc,LocationPhone,LocationCity,LocationState,LocationZip,LocationCode,SEC_Role,ID_Check,Supports_User_Rename,Supports_User_Reopen,Monitored,UnMonitored,DefaultCountry,Date_Format
+$ConsolidatorUsers = import-csv $path$filename|select BusUnit,ComplianceRegion,DefaultPassword,Dept,Effective_Date,@{N='EmailAddress';E={$_.EmailAddress.ToLower()}},Employee_ID,FirstName,HireDate,JobCode,Title,LastName,MiddleName,TaxID,Status,SupervisorID,@{N='UserID';E={$_.EmailAddress.ToLower()}},LocationDesc,LocationPhone,LocationCity,LocationState,LocationZip,LocationCode,SEC_Role,ID_Check,Supports_User_Rename,Supports_User_Reopen,Monitored,UnMonitored,DefaultCountry,Date_Format
 
 #export the csv back to the same location
 $ConsolidatorUsers|export-csv $path$filename -NoTypeInformation
